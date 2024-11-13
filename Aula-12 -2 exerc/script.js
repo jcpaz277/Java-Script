56f139ee14a28125ae719ead5f768cf43d3e3285
Julio Cesar Alves Paz <jcpazpaz1977@gmail.com>
2024-11-13 20:49:10 -0300

    atualização

---
+++ Aula-12 -2 exerc/script.js
@@ -0,0 +1,21 @@
+ function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
+        img.src = 'imagens/fotomanha.png'
+        document.body.style.background = '#fcbf89'
+    }else if (hora >= 12 && hora < 18) {
+        // BOA TARDE!
+         img.src = 'imagens/fototarde.png'
+        document.body.style.background ='#b5b8a9'
+    }else { 
+        // BOA NOITE!
+        img.src = 'imagens/fotonoite.png'
+         document.body.style.background = '#728189'
+    }
+
+ }
